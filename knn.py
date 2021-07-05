bfdb0ab8d4e0b34dcc29098f50056c6da49de404
vvilliam26 <51523303+vvilliam26@users.noreply.github.com>
2021-07-05 16:31:52 -0300

    Updated BFS

--- knn.py
+++ knn.py
@@ -28,11 +28,11 @@ def get_vizinhos(k, coordenada_teste, coordenadas):
 
 class KNN:
 
-    def __init__(self, k, v, dataset):
+    def __init__(self, k, v):
         # self.k = k
         # self.v = v
             #linha abaixo para inserir uma datasheet
-        self.lista_vertices = dataset
+        # self.lista_vertices = dataset
         self.gerar_coordenadas(v)
         self.set_arestas(k)
         self.set_grafo()
